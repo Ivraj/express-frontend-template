e95ced18f4911fe4b33ffc40d60900a0dbba1fe8
Ivraj <iseerha@college.harvard.edu>
2017-07-27 23:52:32 -0400

    testing ejs

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ if (app.get('env') === 'development') app.locals.dev = true;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'app', 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 // uncomment after adding public/favicon.ico
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
